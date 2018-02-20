ffed67de5f67dd3c6bee8f9be07248773638f4c2
Nikolas Magno Braga Leite - AeC <nikolas.leite@grupoaec.com.br>
2018-02-20 16:26:17 -0300

    Adição de single select

--- jquery-weekdays.js
+++ jquery-weekdays.js
@@ -5,6 +5,7 @@ $(function($){
         options.listClass = options.listClass ? options.listClass : 'weekdays-list';
         options.itemClass = options.itemClass ? options.itemClass : 'weekdays-day';
         options.itemSelectedClass = options.itemSelectedClass ? options.itemSelectedClass : 'weekday-selected';
+        options.singleSelect = options.singleSelect ? options.singleSelect : false;
 
         var html = $("<ul class="+options.listClass+">");
 
@@ -14,10 +15,19 @@ $(function($){
             var liElement = $("<li data-day="+index+" class="+options.itemClass+">"+item + "</li>");
 
             liElement.on('click',function(item){
+                if(options.singleSelect){
+                    html.find('li')
+                        .each(function(index,item){ 
+                            var li = $(item);
+                            li.prop('selected', false);
+                            li.removeClass(options.itemSelectedClass);
+                        });
+                }
+
                 var li = $(item.target);
                 var selected = !li.prop('selected')
                 li.prop('selected', selected);
-            li.toggleClass(options.itemSelectedClass);
+                li.toggleClass(options.itemSelectedClass);
             });
 
             html.append(liElement);
@@ -30,17 +40,19 @@ $(function($){
 
     $.fn.selectedIndexes = function(){
         return $(this).find('li')
-            .filter(function(a){ return a.selected; })
-            .map(function(item){ return item["data-day"]; });
+            .filter(function(index,a){ return a.selected; })
+            .map(function(index,item){ return $(item).attr("data-day"); });
     };
 
     $.fn.selectedDays = function(){
         var $this = $(this);
 
         return $(this).find('li')
-            .filter(function(a){ return a.selected; })
-            .map(function(item){ 
-                return $this.data().days[item["data-day"]]; 
+            .filter(function(index,a){ 
+                return a.selected; 
+            })
+            .map(function(index,item){ 
+                return $this.data().days[$(item).attr("data-day")]; 
             });
     }; 
 });
