56ff0d25af9644700a4365c54a4593db1e817327
Nikolas Magno Braga Leite - AeC <nikolas.leite@grupoaec.com.br>
2018-02-21 06:45:04 -0300

    Melhoria de código e adição de single select

--- jquery-weekdays.js
+++ jquery-weekdays.js
@@ -1,47 +1,41 @@
 $(function($){
     $.fn.weekdays = function(options){
-        options = options ? options : {};
-        options.days = options.days ? options.days : $.fn.weekdays.days;
-        options.listClass = options.listClass ? options.listClass : 'weekdays-list';
-        options.itemClass = options.itemClass ? options.itemClass : 'weekdays-day';
-        options.itemSelectedClass = options.itemSelectedClass ? options.itemSelectedClass : 'weekday-selected';
-        options.singleSelect = options.singleSelect ? options.singleSelect : false;
+        options = consolideOptions(options);
 
-        var html = $("<ul class="+options.listClass+">");
+        var $this =  $(this);
+        var $html = $("<ul class="+options.listClass+">");
 
-        $(this).data({ days: options.days});
-
-        $($(this).data().days).each(function(index, item){
-            var liElement = $("<li data-day="+index+" class="+options.itemClass+">"+item + "</li>");
-
-            liElement.on('click',function(item){
-                if(options.singleSelect){
-                    html.find('li')
-                        .each(function(index,item){ 
-                            var li = $(item);
-                            li.prop('selected', false);
-                            li.removeClass(options.itemSelectedClass);
-                        });
-                }
-
-                var li = $(item.target);
-                var selected = !li.prop('selected')
-                li.prop('selected', selected);
-                li.toggleClass(options.itemSelectedClass);
-            });
-
-            html.append(liElement);
+        $this.data({
+            days: options.days
         });
 
-        $(this).append(html); 	
+        $($this.data().days).each(function(index, item){
+            var $liElement = $("<li data-day="+index+" class="+options.itemClass+">"+item + "</li>");
+
+            $liElement.on('click',function(item){
+                if(options.singleSelect)
+                   singleSelectMode(options, $html);
+
+                var $li = $(item.target); 
+                toggleSelection($li, options);
+            });
+
+            $html.append($liElement);
+        });
+
+        $this.append($html); 	
     };
 
     $.fn.weekdays.days = ["Sun","Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]; 
 
     $.fn.selectedIndexes = function(){
         return $(this).find('li')
-            .filter(function(index,a){ return a.selected; })
-            .map(function(index,item){ return $(item).attr("data-day"); });
+            .filter(function(index,a){
+                return a.selected; 
+            })
+            .map(function(index,item){
+                return $(item).attr("data-day"); 
+            });
     };
 
     $.fn.selectedDays = function(){
@@ -55,4 +49,32 @@ $(function($){
                 return $this.data().days[$(item).attr("data-day")]; 
             });
     }; 
+
+    function consolideOptions(options){
+        options = options ? options : {};
+        options.days = options.days ? options.days : $.fn.weekdays.days;
+        options.listClass = options.listClass ? options.listClass : 'weekdays-list';
+        options.itemClass = options.itemClass ? options.itemClass : 'weekdays-day';
+        options.itemSelectedClass = options.itemSelectedClass ? options.itemSelectedClass : 'weekday-selected';
+        options.singleSelect = options.singleSelect ? options.singleSelect : false;
+
+        return options;
+    }
+
+    function singleSelectMode(options, $html){
+         $html.find('li')
+              .each(function(index,item){ 
+                       var $li = $(item);
+
+                       $li.prop('selected', false);
+                       $li.removeClass(options.itemSelectedClass);
+                   });
+    }
+
+    function toggleSelection($li, options){
+         var selected = !$li.prop('selected')
+
+         $li.prop('selected', selected);
+         $li.toggleClass(options.itemSelectedClass);
+    }
 });
